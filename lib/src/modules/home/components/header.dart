b402a107b75ff180d148b99e4e16499f35290307
nguyenkhanh96 <nguyenkhanh96.bk@gmail.com>
2023-01-31 11:06:05 +0700

    #1 - Introduction & make HomePage

---
+++ lib/src/modules/home/components/header.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_2022_movie_ticket_app/src/config/text_style.dart';
+import 'package:flutter_app_2022_movie_ticket_app/src/constants/asset_path.dart';
+
+class HomeHeader extends StatelessWidget {
+  const HomeHeader({
+    Key? key,
+    required this.size,
+  }) : super(key: key);
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(top: 64, left: 24, right: 24),
+      child: SizedBox(
+        height: size.height / 10,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'Find Your Best\nMovie',
+              style: TxtStyle.heading1SemiBold,
+            ),
+            CircleAvatar(
+              radius: size.height / 24,
+              backgroundImage: AssetImage(AssetPath.iconProfile),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
