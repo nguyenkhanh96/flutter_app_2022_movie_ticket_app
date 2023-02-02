f78981ad2ef6fbea6c8a103316af0c8dce636a5c
nguyenkhanh96 <nguyenkhanh96.bk@gmail.com>
2023-02-02 16:04:23 +0700

    #4 - Fancy Carousel Slider List Movie

--- lib/src/modules/home/home_page.dart
+++ lib/src/modules/home/home_page.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app_2022_movie_ticket_app/src/modules/home/components/Category_bar.dart';
+import 'package:flutter_app_2022_movie_ticket_app/src/config/app_color.dart';
+import 'package:flutter_app_2022_movie_ticket_app/src/config/text_style.dart';
+import 'package:flutter_app_2022_movie_ticket_app/src/modules/home/components/category_bar.dart';
 import 'package:flutter_app_2022_movie_ticket_app/src/modules/home/components/header.dart';
 import 'package:flutter_app_2022_movie_ticket_app/src/modules/home/components/search_bar.dart';
+import 'package:flutter_app_2022_movie_ticket_app/src/modules/home/components/slider.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -12,6 +16,7 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             //header
             HomeHeader(size: size),
@@ -19,8 +24,25 @@ class HomePage extends StatelessWidget {
             SearchBar(size: size),
             //category bar
             CategoryBar(size: size),
+            //title
+            buildTitle("Now Playing"),
+            //slider
+            SliderBar(size: size)
           ],
         ),
+      ),
+    );
+  }
+
+  Padding buildTitle(String content) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 24,
+        vertical: 24,
+      ),
+      child: Text(
+        content,
+        style: TxtStyle.heading2,
       ),
     );
   }
