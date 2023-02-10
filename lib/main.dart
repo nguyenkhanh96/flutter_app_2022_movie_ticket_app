8ca38060a7718f40d96ce3f1d742ca33658d621e
nguyenkhanh96 <nguyenkhanh96.bk@gmail.com>
2023-02-10 20:40:37 +0700

    #10 - Change StatusBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_app_2022_movie_ticket_app/src/config/app_color.dart';
 import 'package:flutter_app_2022_movie_ticket_app/src/modules/home/home_page.dart';
 
@@ -12,6 +13,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    //     statusBarColor: Colors.white, statusBarBrightness: Brightness.dark));
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
